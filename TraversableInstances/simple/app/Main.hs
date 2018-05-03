ec9b44e3d5820b67e0f6fbbf564391342ac262cd
Clinton Jenkins <jenkins.333@gmail.com>
2018-05-02 21:00:06 -0600

    More exercises done.

--- TraversableInstances/simple/app/Main.hs
+++ TraversableInstances/simple/app/Main.hs
@@ -30,6 +30,65 @@ instance Arbitrary a => Arbitrary (Optional a) where
 instance Eq a => EqProp (Optional a) where
   (=-=) = eq
 
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = do
+    a <- arbitrary
+    l <- arbitrary
+    frequency [(1, return (Cons a l)), (1, return Nil)]
+
+instance Eq a => EqProp (List a) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three a b c)
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
+
+instance (Eq a, Eq b) => EqProp (Pair a b) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Big a b) where
+  arbitrary = do
+    a <- arbitrary
+    b1 <- arbitrary
+    b2 <- arbitrary
+    return (Big a b1 b2)
+
+instance (Eq a, Eq b) => EqProp (Big a b) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Bigger a b) where
+  arbitrary = do
+    a <- arbitrary
+    b1 <- arbitrary
+    b2 <- arbitrary
+    b3 <- arbitrary
+    return (Bigger a b1 b2 b3)
+
+instance (Eq a, Eq b) => EqProp (Bigger a b) where
+  (=-=) = eq
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = do
+    a <- arbitrary
+    tl <- arbitrary
+    tr <- arbitrary
+    frequency [(1, return Empty), (1, return (Leaf a)), (1, return (Node tl a tr))]
+
+instance (Eq a) => EqProp (Tree a) where
+  (=-=) = eq
+
 main :: IO ()
 main = do
   let trigger :: Identity (Int, Int, [Int])
@@ -38,6 +97,24 @@ main = do
       constantTrigger = undefined
       optionalTrigger :: Optional (Int, Int, [Int])
       optionalTrigger = undefined
+      listTrigger :: List (Int, Int, [Int])
+      listTrigger = undefined
+      threeTrigger :: Three (Int, Int, [Int]) (Int, Int, [Int]) (Int, Int, [Int])
+      threeTrigger = undefined
+      pairTrigger :: Pair (Int, Int, [Int]) (Int, Int, [Int])
+      pairTrigger = undefined
+      bigTrigger :: Big (Int, Int, [Int]) (Int, Int, [Int])
+      bigTrigger = undefined
+      biggerTrigger :: Bigger (Int, Int, [Int]) (Int, Int, [Int])
+      biggerTrigger = undefined
+      treeTrigger :: Tree (Int, Int, [Int])
+      treeTrigger = undefined
   quickBatch (traversable trigger)
   quickBatch (traversable constantTrigger)
   quickBatch (traversable optionalTrigger)
+  quickBatch (traversable listTrigger)
+  quickBatch (traversable threeTrigger)
+  quickBatch (traversable pairTrigger)
+  quickBatch (traversable bigTrigger)
+  quickBatch (traversable biggerTrigger)
+  quickBatch (traversable treeTrigger)
--- TraversableInstances/simple/src/Lib.hs
+++ TraversableInstances/simple/src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib where
 
+import Data.Monoid
+
 newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
 instance Functor Identity where
@@ -49,3 +51,90 @@ instance Foldable Optional where
 instance Traversable Optional where
   traverse f Nada = pure Nada
   traverse f (Yep a) = Yep <$> f a
+
+data List a =
+  Nil
+  | Cons a (List a)
+  deriving (Eq, Ord, Show)
+
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons a l) = Cons (f a) (fmap f l)
+
+instance Foldable List where
+  foldr f initial Nil = initial
+  foldr f initial (Cons a l) = f a $ foldr f initial l
+
+instance Traversable List where
+  traverse f Nil = pure Nil
+  traverse f (Cons a l) = Cons <$> (f a) <*> (traverse f l)
+
+data Three a b c = Three a b c deriving (Eq, Ord, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance Foldable (Three a b) where
+  foldr f initial (Three a b c) = f c initial
+
+instance Traversable (Three a b) where
+  traverse f (Three a b c) = Three a b <$> f c
+
+data Pair a b = Pair a b deriving (Eq, Ord, Show)
+
+instance Functor (Pair a) where
+  fmap f (Pair a b) = Pair a (f b)
+
+instance Foldable (Pair a) where
+  foldr f initial (Pair a b) = f b initial
+
+instance Traversable (Pair a) where
+  traverse f (Pair a b) = Pair a <$> f b
+
+data Big a b = Big a b b deriving (Eq, Ord, Show)
+
+instance Functor (Big a) where
+  fmap f (Big a b1 b2) = Big a (f b1) (f b2)
+
+instance Foldable (Big a) where
+  foldMap f (Big a b1 b2) = mappend (f b1) (f b2)
+
+instance Traversable (Big a) where
+  traverse f (Big a b1 b2) = Big a <$> f b1 <*> f b2
+
+data Bigger a b = Bigger a b b b deriving (Eq, Ord, Show)
+
+instance Functor (Bigger a) where
+  fmap f (Bigger a b1 b2 b3) = Bigger a (f b1) (f b2) (f b3)
+
+instance Foldable (Bigger a) where
+  foldMap f (Bigger a b1 b2 b3) = f b1 <> f b2 <> f b3
+
+instance Traversable (Bigger a) where
+  traverse f (Bigger a b1 b2 b3) = Bigger a <$> f b1 <*> f b2 <*> f b3
+
+data Tree a =
+  Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap f Empty = Empty
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node treeLeft a treeRight) = Node (f <$> treeLeft) (f a) (f <$> treeRight)
+  
+-- foldMap is a bit easier
+-- and looks more natural,
+-- but you can do foldr too
+-- for extra credit.
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node treeLeft a treeRight) = foldMap f treeLeft <> f a <> foldMap f treeRight
+  
+instance Traversable Tree where
+  traverse f Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node treeLeft a treeRight) =
+    Node <$> (traverse f treeLeft) <*> (f a) <*> (traverse f treeRight)
